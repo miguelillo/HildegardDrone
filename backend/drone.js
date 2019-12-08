f49e1f1fae5ad02c0699bf2af8bdcabf1a232855
Miguel Ángel Navarro Vera <manavarro@encamina.com>
2019-12-08 01:25:36 +0100

    First commit

---
+++ backend/drone.js
@@ -0,0 +1,63 @@
+import createSocket from 'dgram';
+import wait from 'waait';
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+import throttle from 'lodash/throttle';
+import delays from './commDelays';
+
+const PORT = 8889;
+const STATE_PORT = 8890;
+const HOST = '192.168.10.1';
+
+const drone = createSocket('udp4');
+drone.bind(PORT);
+
+const droneState = createSocket('udp4');
+droneState.bind(STATE_PORT);
+
+drone.on('message', (message) => {
+    console.log(`🤖 : ${message}`);
+});
+
+function parseState(state) {
+    return state
+        .split(';')
+        .map(x => x.split(':'))
+        .reduce((data, [key, value]) => {
+            data[key] = value;
+            return data;
+        }, {});
+}
+
+function handleError(err) {
+    if (err) {
+        console.log('ERROR');
+        console.log(err);
+    }
+}
+
+const commands = ['command', 'battery?', 'takeoff', 'land'];
+
+drone.send('command', 0, 'command'.length, PORT, HOST, handleError);
+
+io.on('connection', socket => {
+    socket.on('command', command => {
+        console.log('command Sent from browser');
+        console.log(command);
+        drone.send(command, 0, command.length, PORT, HOST, handleError);
+    });
+    socket.emit('status', 'CONNECTED');
+});
+
+droneState.on(
+    'message',
+    throttle(state => {
+        const formattedState = parseState(state.toString());
+        io.sockets.emit('dronestate', formattedState);
+    }, 100)
+);
+
+http.listen(6767, () => {
+    console.log('Socket io server up and running');
+});
---
+++ frontend/components/dronestate.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import Battery from './battery';
+import socket from '../socket';
+import styled from 'styled-components';
+
+function useDroneState() {
+  const [droneState, updateDroneState] = useState({});
+  useEffect(() => {
+    socket.on('dronestate', updateDroneState);
+    return () => socket.removeListener('dronestate');
+  }, []);
+  return droneState;
+}
+
+function useSocket() {
+  const [status, updateStatus] = useState('DISCONNECTED');
+  useEffect(() => {
+    socket.on('status', updateStatus);
+    return () => socket.removeListener('status');
+  }, []);
+  return status;
+}
+const DroneStateStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  grid-gap: 5px;
+  .status {
+    grid-column: 1 / -1;
+    text-align: center;
+  }
+`;
+
+const DroneState = () => {
+  const status = useSocket();
+  const droneState = useDroneState([]);
+  return (
+    <DroneStateStyles>
+      <p className="status">Status: {status}</p>
+      <Battery battery={droneState.bat} />
+    </DroneStateStyles>
+  );
+};
+
+
+export default DroneState;
